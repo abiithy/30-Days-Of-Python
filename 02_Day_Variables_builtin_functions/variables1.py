2fdd7fc1b81d56408281fb42120f8649b625a78c
abiithy <abirip2004@hotmail.com>
2024-12-02 08:19:57 +1100

    Update variables1.py

--- 02_Day_Variables_builtin_functions/variables1.py
+++ 02_Day_Variables_builtin_functions/variables1.py
@@ -1,1 +1,13 @@
+#day 2 30 days of python programming
 
+first_name = "abi"
+last_name = "rippard"
+full_name = "abi rippard"
+country = "australia"
+city = "sydney"
+age = "20"
+year = "2024"
+is_married = 'not married'
+num1 = "1" num2 = '2'
+
+print(first_name)
